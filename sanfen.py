c558acdb68f85b1adf02e2e542f8773f5e94c4a2
mabenghuayi99 <nihaoyaya@gmx.com>
2026-09-01 07:15:38 +0800

    Update LOGIN_URL to include '/login' path

--- sanfen.py
+++ sanfen.py
@@ -2,7 +2,7 @@ import os
 import requests
 import time
 
-LOGIN_URL = "https://sanfen.gonghailin.xin"
+LOGIN_URL = "https://sanfen.gonghailin.xin/login"
 USER_FIELD = "email"
 PWD_FIELD = "password"
 REQUEST_INTERVAL = 2
